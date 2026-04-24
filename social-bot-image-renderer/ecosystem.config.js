00e760a90febe300f0433006d573e7fa70372dcc
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2026-04-24 22:50:11 +0100

    fix(social-image-renderer): add playwright path

--- social-bot-image-renderer/ecosystem.config.js
+++ social-bot-image-renderer/ecosystem.config.js
@@ -3,7 +3,10 @@ export default {
     {
       name: "image-renderer",
       script: "server/index.js",
-      cwd: "/etc/sewagedata-image-renderer"
+      cwd: "/etc/sewagedata-image-renderer",
+      env: {
+        PLAYWRIGHT_BROWSERS_PATH: "/etc/sewagedata-image-renderer/.playwright"
+      }
     }
   ]
 }
