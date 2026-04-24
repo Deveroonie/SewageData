49ae203a7cf9fb80c36aa33092c1ec3a3f595df4
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2026-04-24 22:54:30 +0100

    fix(social-image-renderer): use correct syntax for cjs

--- social-bot-image-renderer/ecosystem.config.cjs
+++ social-bot-image-renderer/ecosystem.config.cjs
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "image-renderer",
-      script: "server/index.js",
+      script: "/etc/sewagedata-image-renderer/server/index.js",
       cwd: "/etc/sewagedata-image-renderer",
       env: {
         PLAYWRIGHT_BROWSERS_PATH: "/etc/sewagedata-image-renderer/.playwright"
