cc355d12dacd19b443457114964a2c0e03e9d19f
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2026-08-14 18:12:19 +0100

    fix(image-renderer): fix browsers path

--- social-bot-image-renderer/ecosystem.config.cjs
+++ social-bot-image-renderer/ecosystem.config.cjs
@@ -4,9 +4,6 @@ module.exports = {
       name: "image-renderer",
       script: "/etc/sewagedata-image-renderer/server/index.js",
       cwd: "/etc/sewagedata-image-renderer",
-      env: {
-        PLAYWRIGHT_BROWSERS_PATH: "/etc/sewagedata-image-renderer/.playwright"
-      }
     }
   ]
 }
